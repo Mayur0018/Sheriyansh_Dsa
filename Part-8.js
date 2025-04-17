e40d6b682d2832f32aa369347dad1c438a6658ef
Mayur0018 <mayurnish18@gmail.com>
2025-04-17 14:51:59 +0530

    commmit

--- Part-8.js
+++ Part-8.js
@@ -1,6 +1,25 @@
-for (let i = 5; i >= 1; i--) {
-  for (let j = 1; j <= i; j++) {
-    process.stdout.write("*");
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= 5-i;j++) {
+//     process.stdout.write(" ");
+//   }
+//   for(let j =1; j<=i; j++){
+//     process.stdout.write("*")
+//   }
+//   console.log();
+// }
+
+// x patten
+
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n; j++) {
+    if (i == j || i + j == n + 1) {
+      process.stdout.write("*");
+    } else {
+      process.stdout.write("  ");
+    }
   }
+
   console.log();
 }
