2fa6dc04f967a235e7c3603bb194fd2ef4443dec
Mayur0018 <mayurnish18@gmail.com>
2025-04-17 12:53:03 +0530

    commit

--- Part-8.js
+++ Part-8.js
@@ -1,7 +1,6 @@
-for (let i = 0; i <= 5; i++) {
-  for (let j = 0; j < i; j++) {
-    process.stdout.write("*")
+for (let i = 5; i >= 1; i--) {
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("*");
   }
   console.log();
-  
 }
