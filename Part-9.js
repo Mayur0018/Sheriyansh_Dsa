a6f209dd2a9ffe94639bdd5109858c8a76c77d69
Mayur0018 <mayurnish18@gmail.com>
2025-04-29 21:14:03 +0530

    commit

--- Part-9.js
+++ Part-9.js
@@ -1,1 +1,16 @@
-// bubble sort 
+// bubble sort
+let arr = [10, 20, 30, 40, 50];
+let n = arr.length;
+console.log(n);
+
+for (let i = 0; i < n - 1 - i; i++) {
+  for(let j =0; j<n-1-i; j++){
+    if(arr[j]>arr[j+1]){
+        let temp = arr[j]
+        arr[j] = arr[j+1];
+        arr[j+1] = temp
+    }
+  }
+}
+
+console.log(arr);
