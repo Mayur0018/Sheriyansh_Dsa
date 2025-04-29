8294dfb3b58b2d5db2dd28c63541742bcf96e317
Mayur0018 <mayurnish18@gmail.com>
2025-04-29 18:03:30 +0530

    commit

--- Part-8.js
+++ Part-8.js
@@ -10,16 +10,20 @@
 
 // x patten
 
-let n = 5;
+// let n = 5;
 
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= n; j++) {
-    if (i == j || i + j == n + 1) {
-      process.stdout.write("*");
-    } else {
-      process.stdout.write("  ");
-    }
-  }
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     if (i == j || i + j == n + 1) {
+//       process.stdout.write("*");
+//     } else {
+//       process.stdout.write("  ");
+//     }
+//   }
 
-  console.log();
-}
+//   console.log();
+// }
+
+// v pattern
+
+    
