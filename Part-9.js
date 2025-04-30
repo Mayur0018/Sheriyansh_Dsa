eca1561852069c68538e79a3ebc4c6cf1a2e2712
Mayur0018 <mayurnish18@gmail.com>
2025-04-30 23:31:25 +0530

    commit

--- Part-9.js
+++ Part-9.js
@@ -1,11 +1,23 @@
-let arr = [10, 5, 12, 1, 3];
-let n = arr.length;
-for (let i = 0; i < n - 1; i++) {
-  for (let j = 0; j < n - 1 - i; j++) {
-    if(arr[j]>arr[j+1]){
-      let temp = arr[j];
-      arr[j] = arr[j+1];
-      arr[j+1] = temp;
+// let arr = [10, 5, 12, 1, 3];
+// let n = arr.length;
+// for (let i = 0; i < n - 1; i++) {
+//   for (let j = 0; j < n - 1 - i; j++) {
+//     if(arr[j]>arr[j+1]){
+//       let temp = arr[j];
+//       arr[j] = arr[j+1];
+//       arr[j+1] = temp;
+//     }
+//   }
+// }
+
+function findMaxNumber(arr) {
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
   }
+  return max;
 }
+const numbers = [18, 45, 33, 7];
+console.log(findMaxNumber(numbers));
