4f19c233bb8fd343e93a780c10b669795ea68bb5
Mayur0018 <mayurnish18@gmail.com>
2025-05-05 20:08:06 +0530

    comiit

--- Part-9.js
+++ Part-9.js
@@ -1,8 +1,9 @@
 let arr = [10, 5, 12, 1, 3];
 let n = arr.length;
 for (let i = 0; i < n - 1; i++) {
-  for (let j = 0; j < n - 1 - i; j++) {
-    if (arr[j] > arr[j + 1]) {
+  let minIndex = i;
+  for (let j = 1; j < n; j++) {
+    if (arr[minIndex] > arr[j] ) {
       let temp = arr[j];
       arr[j] = arr[j + 1];
       arr[j + 1] = temp;
