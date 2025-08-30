33393064d7a744f714bed814cf8c00cd1230e128
Mayur0018 <mayurnish18@gmail.com>
2025-08-30 18:04:13 +0530

    commit

---
+++ Master The Basic/index.js
@@ -0,0 +1,16 @@
+// let a = 12;
+// let b = "13";
+// console.log(a+b); output 1213
+// type cohrcen
+
+// swaping of tow values
+
+let a = 100;
+let b = 200;
+let c;
+
+c = a;
+a = b;
+b = c;
+
+console.log(a, b);
