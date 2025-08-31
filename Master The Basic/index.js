b3e91d908037b1343f9d3afac5efa1820e6e681b
Mayur0018 <mayurnish18@gmail.com>
2025-08-31 17:40:23 +0530

    comitt

--- Master The Basic/index.js
+++ Master The Basic/index.js
@@ -5,12 +5,24 @@
 
 // swaping of tow values
 
-let a = 100;
-let b = 200;
-let c;
+// let a = 100;
+// let b = 200;
+// let c;
 
-c = a;
-a = b;
-b = c;
+// c = a;
+// a = b;
+// b = c;
 
-console.log(a, b);
+// console.log(a, b);
+
+// \ this use for create divison for any values 
+// % this use for get remainder of any values 
+
+// ! this check not equal to left to right
+
+// let a =10;
+// let b = a++;
+// console.log(a);
+// console.log(b);
+
+
