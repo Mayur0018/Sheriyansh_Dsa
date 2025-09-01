757b056f324d55a0208515b2fec28bc6d76329e4
Mayur0018 <mayurnish18@gmail.com>
2025-09-02 01:02:23 +0530

    commit

---
+++ PatternPrograming/index.js
@@ -0,0 +1,31 @@
+// printing 5 star pattern
+// let prompt = require("prompt-sync")();
+// let a = prompt("Enter The Number");
+// console.log(a);
+// let n = Number(prompt("Enter The Number"));
+// for (let i = 1; i < n; i++) {
+//   for (let j = 1; j < n; j++) {
+//     process.stdout.write("*");
+//   }
+//   console.log();
+// }
+
+// let m = prompt("Enter The Number");
+// for (let i = 1; i < m; i++) {
+//   for (let j = 1; j < m; j++) {
+//     document.write("*");
+//   }
+//   document.write("<br>");
+// }
+
+// right angle triangle pattern
+// let prompt = require("prompt-sync")();
+// let n = prompt("Enter The Number");
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write("*");
+//   }
+//   console.log();
+// }
+
+
