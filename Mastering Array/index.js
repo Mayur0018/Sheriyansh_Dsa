7bc029e7af10eb40c6a6ebb186b9b3f8181b2669
Mayur0018 <mayurnish18@gmail.com>
2025-09-02 18:25:02 +0530

    comitt

---
+++ Mastering Array/index.js
@@ -0,0 +1,6 @@
+// array is the liner data structure thats store multiple values
+let box = 0;
+for(let i =0; i<=10; i++){
+    box = box + i;
+}
+console.log(box);
--- PatternPrograming/index.js
+++ PatternPrograming/index.js
@@ -46,15 +46,23 @@ let n = Number(prompt("Enter The Number"));
 //   console.log();
 
 // }
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= n; j++) {
-    if (i == j || i + j == n + 1) {
-      process.stdout.write("* ");
-    } else {
-      process.stdout.write("  ");
-    }
-  }
-  console.log();
-}
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     if (i == j || i + j == n + 1) {
+//       process.stdout.write("* ");
+//     } else {
+//       process.stdout.write("  ");
+//     }
+//   }
+//   console.log();
+// }
 
-
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j < 2*n-1; j++) {
+//     if (i == j || i + j == 2*n) {
+//       process.stdout.write("* ");
+//     }
+//     process.stdout.write("  ");
+//   }
+//   console.log();
+// }
