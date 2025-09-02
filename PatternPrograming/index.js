31dae65b784bc95dfd8ed338575552d77ea34e21
Mayur0018 <mayurnish18@gmail.com>
2025-09-02 12:05:07 +0530

    committ

--- PatternPrograming/index.js
+++ PatternPrograming/index.js
@@ -19,13 +19,42 @@
 // }
 
 // right angle triangle pattern
-// let prompt = require("prompt-sync")();
-// let n = prompt("Enter The Number");
+let prompt = require("prompt-sync")();
+let n = Number(prompt("Enter The Number"));
 // for (let i = 1; i <= n; i++) {
 //   for (let j = 1; j <= i; j++) {
 //     process.stdout.write("*");
 //   }
 //   console.log();
 // }
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i + 1; j++) {
+//     process.stdout.write("*");
+//   }
+//   console.log();
+// }
+
+// mirror rightangle pattern
+
+// for(let i = 1; i<=n; i++){
+//   for(let j = 1; j<=n-i+1; j++){
+//     process.stdout.write(" ");
+//   }
+//   for(let j = 1; j<=i; j++){
+//     process.stdout.write("*");
+//   }
+//   console.log();
+
+// }
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n; j++) {
+    if (i == j || i + j == n + 1) {
+      process.stdout.write("* ");
+    } else {
+      process.stdout.write("  ");
+    }
+  }
+  console.log();
+}
 
 
