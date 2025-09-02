0a12080a8bf344b9a39add186c1850fba986db44
Mayur0018 <mayurnish18@gmail.com>
2025-09-03 00:48:39 +0530

    comitt

--- Mastering Array/index.js
+++ Mastering Array/index.js
@@ -21,16 +21,30 @@
 
 // find second largest of Array
 
-let arr = [10, 20, 76, 80, 40, 100];
-let maxvalue = Math.max(arr[0], arr[1]);
-let minvalue = Math.min(arr[0], arr[1]);
+// let arr = [10, 20, 76, 80, 40, 100];
+// let maxvalue = Math.max(arr[0], arr[1]);
+// let minvalue = Math.min(arr[0], arr[1]);
 
-for (let i = 2; i < arr.length; i++) {
-  if (arr[i] > maxvalue) {
-    minvalue = maxvalue;
-    maxvalue = arr[i];  
-  } else if (arr[i] > minvalue && maxvalue !== arr[i]) {
-    minvalue = arr[i];
-  }
+// for (let i = 2; i < arr.length; i++) {
+//   if (arr[i] > maxvalue) {
+//     minvalue = maxvalue;
+//     maxvalue = arr[i];
+//   } else if (arr[i] > minvalue && maxvalue !== arr[i]) {
+//     minvalue = arr[i];
+//   }
+// }
+// console.log(minvalue);
+
+// reverse Array with extra space
+
+let arr = [10, 20, 30, 40, 50, 60];
+let i = 0, j = arr.length - 1;
+
+while (i != j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+  i++;
+  j--;
 }
-console.log(minvalue);
+console.log(arr);
