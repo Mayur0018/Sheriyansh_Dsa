513db21683f1d863be04de58ef772d0ef3804a44
Mayur0018 <mayurnish18@gmail.com>
2025-09-03 18:40:28 +0530

    commit

--- Mastering Array/index.js
+++ Mastering Array/index.js
@@ -94,3 +94,15 @@
 // }
 // console.log(arr);
 // console.log(count);
+// let arr = [1, 2, 3, 4, 5];
+// let temp = new Array(arr.length);
+// let k = Number(prompt("Enter Ther K value"));
+// k = k % arr.length;
+
+// for (let i = 0; i < arr.length; i++) {
+//   temp[i] = arr[(i + k) % arr.length];
+// }
+// console.log(temp);
+
+// remove dulicates from the sorted array 
+
