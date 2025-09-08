d559ca525e0655435b8d6c4c62ee549468ecac43
Mayur0018 <mayurnish18@gmail.com>
2025-09-08 18:30:20 +0530

    Master String

--- Mastering Array/index.js
+++ Mastering Array/index.js
@@ -219,4 +219,19 @@ var trap = function (height) {
   let maxLeft = height[0],
     maxRight = height[height.length - 1];
   (left[0] = maxLeft), (right[right.length - 1] = maxRight);
+
+  for (let i = 1; i < height.length; i++) {
+    maxLeft = Math.max(height[i], maxLeft);
+    left[i] = maxLeft;
+  }
+  for (let i = height.length - 2; i > 0; i--) {
+    maxRight = Math.max(height[i], maxRight);
+    right[i] = maxRight;
+  }
+  let ans = 0;
+  for (let i = 0; i < height.length; i++) {
+    ans += Math.min(left[i], right[i] - height[i]);
+  }
+  return ans
 };
+console.log(trap[10,20,30,40,50]);
