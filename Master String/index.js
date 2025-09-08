82a72a60d040306f025c6aee3f85f9a739dd136c
Mayur0018 <mayurnish18@gmail.com>
2025-09-08 19:15:20 +0530

    committ

---
+++ Master String/index.js
@@ -0,0 +1,14 @@
+// let username = "ali";
+// console.log(username);
+// let s = "mayur";
+// console.log(s.length);
+
+// let username = "mayurNishad";
+// for(let i =0; i<username.length;i++){
+//   console.log(username[i]);
+// let captilize =   username[i].toUpperCase();
+//   console.log(captilize);
+  
+// }
+
+
