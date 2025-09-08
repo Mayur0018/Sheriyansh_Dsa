fc78b224d012f46b4b7e418be628196dc28f79da
Mayur0018 <mayurnish18@gmail.com>
2025-09-08 20:23:22 +0530

    comit

--- Master String/index.js
+++ Master String/index.js
@@ -8,7 +8,23 @@
 //   console.log(username[i]);
 // let captilize =   username[i].toUpperCase();
 //   console.log(captilize);
-  
+
+// }
+// function reverseString( username){
+// for (let index = username.length - 1; index >= 0; index--) {
+//   console.log(username[index]);
+// }
+// }
+// console.log(reverseString("Mayur Nishad"));
+// function userString(username){
+//     let reverse = "";
+//     for(let i =username.length -1; i>=0; i--){
+//         reverse += username[i];
+//     }
+//     return reverse;
 // }
 
+// console.log(userString("Mayur"));
+
+// palidrome of String
 
