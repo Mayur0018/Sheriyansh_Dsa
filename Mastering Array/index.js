02caae4963dce8d6b03a73cc9152753cfced2a25
Mayur0018 <mayurnish18@gmail.com>
2025-10-21 12:01:41 +0530

    Solving Dsa Problems

--- Mastering Array/index.js
+++ Mastering Array/index.js
@@ -584,9 +584,11 @@
 //     return ans;
 // }
 
-
-
-
-
-
-
+function reverseString(str) {
+  let reverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  return reverse;
+}
+console.log(reverseString("Mayur"));
