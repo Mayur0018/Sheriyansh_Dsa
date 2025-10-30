0fab46bdb819e73e6b50a020173e6544a542c5b3
Mayur0018 <mayurnish18@gmail.com>
2025-10-30 14:09:35 +0530

    pattern Programing Pretice

--- PatternPrograming/index.js
+++ PatternPrograming/index.js
@@ -67,18 +67,22 @@
 //   console.log();
 // }
 
-
 // process.stdout.write("Hellow");
 // process.stdout.write("Mayur Nishad is The Best Coder");
 
+// process.stdout.write("*");
+// process.stdout.write("*");
+// process.stdout.write("*");
+// process.stdout.write("*");
+// process.stdout.write("*");
+// process.stdout.write("*");
+// process.stdout.write("*");
+// process.stdout.write("*");
+// process.stdout.write("*");
 
-
-process.stdout.write("*");
-process.stdout.write("*");
-process.stdout.write("*");
-process.stdout.write("*");
-process.stdout.write("*");
-process.stdout.write("*");
-process.stdout.write("*");
-process.stdout.write("*");
-process.stdout.write("*");
+for (let i = 0; i <= 5; i++) {
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("*");
+  }
+  process.stdout.write("\n");
+}
