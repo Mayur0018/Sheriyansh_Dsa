c09a128b1400f4054fea647b95bf624c4f52f3cb
Mayur0018 <mayurnish18@gmail.com>
2025-12-12 14:24:16 +0530

    Logic Building

--- Dsa Prectice/index.js
+++ Dsa Prectice/index.js
@@ -50,13 +50,123 @@ function findMax(str) {
 console.log(findMax([10, 20, 30, 40, 50]));
 
 // Write a function to return the smallest element in an array.
-function findMin(str){
+function findMin(str) {
   let min = str[0];
-  for(let i =0; i<str.length; i++){
-    if(str[i]<min){
-      min = str[i]
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] < min) {
+      min = str[i];
     }
   }
   return min;
 }
-console.log(findMin([10,20,8,30]));
+console.log(findMin([10, 20, 8, 30]));
+
+// Return count of even numbers in an array.
+
+function CountEvenNumers(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] % 2 === 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(CountEvenNumers([10, 50, 55, 11]));
+
+// Check The Palidrome Number
+function Palidrome(str) {
+  let reverseString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseString = reverseString + str[i];
+    if (str === reverseString) {
+      console.log("String is Palindrome");
+    } else {
+      console.log("String is Not Palindrome");
+    }
+  }
+  return reverseString;
+}
+
+console.log(Palidrome("aa"));
+
+// Remove Duplicates From Array
+function RemoveDuplicates(str) {
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    let isduplicate = false;
+    for (let j = 0; j < result.length; j++) {
+      if (str[i] === result[j]) {
+        isduplicate = true;
+        break;
+      }
+    }
+    if (!isduplicate) {
+      result.push(str[i]);
+    }
+  }
+  return result.join("");
+}
+console.log(RemoveDuplicates("Mayurrrrrrrrrrr"));
+
+// Sum Of Array Elements
+function sumOfArray(sumofvalues) {
+  let sum = 0;
+  for (let i = 0; i < sumofvalues.length - 1; i++) {
+    sum = sum + sumofvalues[i];
+  }
+  return sum;
+}
+console.log(sumOfArray([10, 20, 30]));
+
+// find the second largerst Number
+function secondLrgest(arr) {
+  let max = -Infinity;
+  let second = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      second = max;
+      max = arr[i];
+    } else if (arr[i] > second && arr[i] !== max) {
+      second = arr[i];
+    }
+  }
+  return second;
+}
+console.log(secondLrgest([10, 30, 40, 50, 70]));
+
+// Count Total digit in Number
+function countDigits(nums) {
+  let count = 0;
+  while (nums > 0) {
+    nums = Math.floor(nums / 10);
+    count++;
+  }
+  return count;
+}
+console.log(countDigits(1000));
+
+// Find The Factorial in Number
+function findFactorial(nums) {
+  let result = 1;
+  for (let i = 1; i < nums; i++) {
+    result = result * i;
+  }
+  return result;
+}
+console.log(findFactorial(4));
+
+// find Number is prime
+function isprime(nums) {
+  if (nums < 1) return false;
+  for (let i = 0; i < nums; i++) {
+    if (nums % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(isprime(3));
+
+// Find Fibonachii Series 
