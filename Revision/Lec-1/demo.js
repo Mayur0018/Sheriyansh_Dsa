f7ff58c21f73a1a97250da60251e599e59e07048
Mayur0018 <mayurnish18@gmail.com>
2026-09-01 12:48:01 +0530

    Add initial implementations for array manipulation problems

---
+++ Revision/Lec-1/demo.js
@@ -0,0 +1,30 @@
+// let a = 10;
+// let b = 20;
+// console.log("The value of a is: " + a + b);
+
+// console.log("The value of a is: " + (a + b));
+
+// let c;
+// c = a;
+// a = b;
+// b = c;
+
+// console.log("The value of a is: " + c);
+
+// [a, b] = [b, a];
+// console.log(a, b);
+
+// let a = 7;
+// let b = 2;
+
+// console.log(b % a);
+
+// let c = [10, 20, 30, 40, 50];
+// let reverse;
+
+// for (let i = 0; i < c.length; i++) {
+//   reverse = reverse + c[i];
+//   console.log(reverse);
+// }
+
+
